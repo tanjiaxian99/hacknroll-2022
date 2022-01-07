7dce11cba5c32be9423fea3e024221f0c3ba1c35
downmeanhigh <daominhhai@u.nus.edu>
2022-01-08 07:11:08 +0800

    final commit. Commit #7

--- bot.py
+++ bot.py
@@ -459,19 +459,13 @@ def main() -> None:
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
-                          url_path=TOKEN, 
-                          webhook_url='https://floating-earth-81212.herokuapp.com/' + TOKEN)
+                          url_path=API_TOKEN, 
+                          webhook_url='https://floating-earth-81212.herokuapp.com/' + API_TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
 
 if __name__ == '__main__':
     main()
